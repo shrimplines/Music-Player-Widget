c5b5b42cd32cb0bfaa7cdb2261e6b4e64dae9f2e
Shrimp <one.walmart.bag@gmail.com>
2026-02-12 12:53:14 -0800

    Update renderer.js

--- src/renderer.js
+++ src/renderer.js
@@ -1,13 +1,12 @@
 const titleTag = document.getElementById('song-title');
 const artistTag = document.getElementById('artist-name');
 
-// Update UI from Spotify
+// UI from spotify
 window.electronAPI.onTrackChange((data) => {
   titleTag.innerText = data.title;
   artistTag.innerText = data.artist;
 });
 
-// Controls
 document.getElementById('play-btn').onclick = () => window.electronAPI.sendControl('play-pause');
 document.getElementById('next').onclick = () => window.electronAPI.sendControl('next');
 document.getElementById('prev').onclick = () => window.electronAPI.sendControl('prev');
